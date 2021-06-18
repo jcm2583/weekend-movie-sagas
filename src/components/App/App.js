77f5482d6793af7686459a8a81f7329cb0619e15
Jacob Motes <jmotes2583@gmail.com>
2021-06-18 18:28:45 -0500

    path added on app page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,7 @@ function App() {
         </Route>
         
         {/* Details page */}
-      <MovieDetails />
+      <MovieDetails path='/details'/>
         {/* Add Movie page */}
       </Router>
     </div>
